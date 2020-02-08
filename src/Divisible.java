39eeb4c9cb0c8f802b13537f9231a3e3101b080e
Michał Szymecki <szymsonik@gmail.com>
2020-02-08 01:00:29 +0100

    create project

---
+++ src/Divisible.java
@@ -0,0 +1,20 @@
+public class Divisible {
+    public static void main(String[] args) {
+        boolean flag = true;
+        int i;
+        for (i=1; ; i++) {
+            flag=true;
+            for (int d = 1; d <= 20; d++) {
+                if (i % d != 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                break;
+        }
+    }
+        System.out.println(i);
+
+}
+}
